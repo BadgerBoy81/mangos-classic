7ff5c737ed14f42c0f894a62c06eb4c7d77f7ecc
killerwife <killerwife@gmail.com>
2023-05-27 18:52:33 +0200

    [z2812] Creature: Merge visibility back to static flags where it initially originated

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -3,5 +3,9 @@
  #define REVISION_DB_REALMD "required_z2800_01_realmd_platform"
  #define REVISION_DB_LOGS "required_z2778_01_logs_anticheat"
  #define REVISION_DB_CHARACTERS "required_z2799_01_characters_account_data"
+<<<<<<< HEAD
  #define REVISION_DB_MANGOS "required_z2813_01_mangos_trainer_ability"
+=======
+ #define REVISION_DB_MANGOS "required_z2812_01_mangos_visibility"
+>>>>>>> 565568dab ([z2812] Creature: Merge visibility back to static flags where it initially originated)
 #endif // __REVISION_SQL_H__
