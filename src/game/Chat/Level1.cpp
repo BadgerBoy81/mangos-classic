cmangos/mangos-tbc

Chat: Disallow tping to bg or arena using go xyz command

src/game/Chat/Level1.cpp
@@ -1856,7 +1856,15 @@ bool ChatHandler::HandleGoXYZCommand(char* args)
     float z = (float)atof(pz);
     uint32 mapid;
     if (pmapid)
+    {
         mapid = (uint32)atoi(pmapid);
+        MapEntry const* mapEntry = sMapStore.LookupEntry(mapid);
+        if (!mapEntry || mapEntry->IsBattleGroundOrArena())
+        {
+            PSendSysMessage("Map %u is battleground or arena. Not allowed through XYZ command.", mapid);
+            return false;
+        }
+    }
     else
         mapid = _player->GetMapId();
 

BadgerBoy81/mangos-classic

src/game/Chat/Level1.cpp
@@ -1791,7 +1791,15 @@ bool ChatHandler::HandleGoXYZCommand(char* args)
     float z = (float)atof(pz);
     uint32 mapid;
     if (pmapid)
+    {
         mapid = (uint32)atoi(pmapid);
+        MapEntry const* mapEntry = sMapStore.LookupEntry(mapid);
+        if (!mapEntry || mapEntry->IsBattleGround())
+        {
+            PSendSysMessage("Map %u is battleground or arena. Not allowed through XYZ command.", mapid);
+            return false;
+        }
+    }
     else
         mapid = _player->GetMapId();
 